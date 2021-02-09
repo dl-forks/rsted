03397d91ebcf5cd663da2fcae4c983fd7121860d
Javier Llopis <javier@llopis.me>
2021-02-09 12:08:03 +0000

    Fix Python3 compatibility error on pdf write

--- rsted/pdf.py
+++ rsted/pdf.py
@@ -6,16 +6,17 @@ utf8codec = codecs.lookup('utf-8')
 from flask import current_app
 
 if sys.version_info > (3,):
-    from io import StringIO
+    #from io import StringIO
+    from io import BytesIO
 else:
     try:
-        from cStringIO import StringIO
+        from cStringIO import StringIO as BytesIO
     except ImportError:
-        from StringIO import StringIO
+        from StringIO import StringIO as BytesIO
 
 def rst2pdf(content, theme=None):
     topdf = RstToPdf(basedir=current_app.config.root_path, breaklevel=0)
-    buf = StringIO()
+    buf = BytesIO()
 
     if not content:
         content = '\0'
