45a9769c6589ded48e762c6b02732814a80b2875
Javier Llopis <javier@llopis.me>
2021-02-09 10:46:24 +0000

    Changes for compatibility with python3

--- rsted/pdf.py
+++ rsted/pdf.py
@@ -1,13 +1,17 @@
+import sys
 from rst2pdf.createpdf import RstToPdf
 import codecs
 utf8codec = codecs.lookup('utf-8')
 
 from flask import current_app
 
-try:
-    from cStringIO import StringIO
-except ImportError:
-    from StringIO import StringIO
+if sys.version > (3,):
+    from io import StringIO
+else:
+    try:
+        from cStringIO import StringIO
+    except ImportError:
+        from StringIO import StringIO
 
 def rst2pdf(content, theme=None):
     topdf = RstToPdf(basedir=current_app.config.root_path, breaklevel=0)
